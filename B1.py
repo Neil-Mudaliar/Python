ca347d8f7118bd31c6dcdb0a180d47550b433073
NEIL MUDALIAR <neilmudaliarr@gmail.com>
2026-03-15 19:22:11 +0530

    Beginner PS 1

---
+++ B1.py
@@ -0,0 +1,4 @@
+#Write a program that asks for a user's name and age, then prints how old they'll be in 10 years.
+name = str(input("Enter your name = "))
+age = int(input("Enter your age = "))
+print(name + " , Your age in 10 years will be " , age+10 , ".")
