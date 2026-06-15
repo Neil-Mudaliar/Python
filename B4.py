9fdc9d0735dcac5562404e9d67508a440afca2ed
NEIL MUDALIAR <neilmudaliarr@gmail.com>
2026-06-15 21:03:55 +0530

    Beginner PS 4

---
+++ B4.py
@@ -0,0 +1,10 @@
+#Create a function that checks if a number is prime and returns True or False.
+num=int(input("Enter a number = "))
+while 1:
+    for i in range (2,num):
+        if num%i==0:
+            print("The number is not prime.")
+            break
+        else:
+            print("The number is prime.")
+            break
