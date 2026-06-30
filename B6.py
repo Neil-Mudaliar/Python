34c80a22489d3501ecd7df3af8af3aa036295584
NEIL MUDALIAR <neilmudaliarr@gmail.com>
2026-06-30 12:30:23 +0530

    Beginner PS 6

---
+++ B6.py
@@ -0,0 +1,9 @@
+#Build a number guessing game: the computer picks a random number 1–10, the user guesses until correct.
+import random
+num = int(input("Enter a number between 1 and 10 = "))
+num2 = random.randint(1,10)
+if num == num2:
+    print("You guessed correctly ",num2)
+else :
+    print("Try again later")
+    print(num2)
