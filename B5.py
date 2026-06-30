d92eba247b38a0dfa47376ca218731897b41aa08
NEIL MUDALIAR <neilmudaliarr@gmail.com>
2026-06-30 12:31:48 +0530

    Beginner PS 5

--- B5.py
+++ B5.py
@@ -1,6 +1,12 @@
 #Write a program that counts how many vowels are in a string entered by the user.
-sentence = str(input("Enter a sentence or a word = "))
-if sentence.count("a")>0:
-    print("The number of time a has occured is ",sentence.count("a"))
+sentence = str(input("Enter a sentence ="))
+num1 = sentence.count("a")
+num2 = sentence.count("e")
+num3 = sentence.count("i")
+num4 = sentence.count("o")
+num5 = sentence.count("u")
+num = num1 + num2 + num3 + num4 + num5
+if num>0:
+    print("Total number of vowels in the sentence are ",num)
 else:
-    print("There is no a in the sentence.")
+    print("There are no vowels in the sentence")
